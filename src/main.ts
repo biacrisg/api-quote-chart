f063e1ccbe623892c7aa6b868e54d2f62064789f
Beatriz Gonçalves <68661352+biacrisg@users.noreply.github.com>
2024-12-02 17:44:30 -0300

    port main

--- src/main.ts
+++ src/main.ts
@@ -3,13 +3,16 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const port = process.env.PORT || 8080;
 
   app.enableCors({
     origin: 'https://quote-chart-frontend-lbs7.vercel.app',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
+    preflightContinue: false,
   });
 
-  await app.listen(8080);
+  await app.listen(port);
+  console.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
